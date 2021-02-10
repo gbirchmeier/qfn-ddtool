48107d5a10220e624b02e77903d571be9fb65f55
Grant Birchmeier <gbirchmeier@connamara.com>
2021-02-10 10:00:45 -0600

    generate FieldTags.cs

---
+++ DDTool/Generators/GenFields.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace DDTool.Generators
+{
+    public class GenFields
+    {
+    }
+}
--- DDTool/Program.cs
+++ DDTool/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using System.Linq;
+using System.Xml;
+using System.IO;
+using System;
 using System.Collections.Generic;
 using DDTool.Parsers;
 using DDTool.Structures;
@@ -28,9 +31,14 @@ namespace DDTool
             {
                 Console.WriteLine("No output dir was specified, so I won't generate anything.");
             }
+            else if (!Directory.Exists(options.OutputDir))
+            {
+                Console.WriteLine($"OutputDir does not exist: {options.OutputDir}");
+                Environment.Exit(1);
+            }
             else
             {
-                Console.WriteLine($"If codegen was supported, I'd write it to: {options.OutputDir}");
+                Console.WriteLine($"Generated files will be written to: {options.OutputDir}");
             }
 
             var errors = new List<string>();
@@ -39,7 +47,7 @@ namespace DDTool
             foreach (DataDictionary dd in dds)
             {
                 List<string> ddErrors = DDTool.Validations.FieldValidator.Check(dd);
-                foreach(string dde in ddErrors)
+                foreach (string dde in ddErrors)
                     errors.Add($"{dd.SourceFile}: {dde}");
             }
 
@@ -47,8 +55,21 @@ namespace DDTool
             {
                 Console.WriteLine("============================");
                 Console.WriteLine("Errors found.  Code generation (if commanded) will not run.");
-                foreach(var err in errors)
+                foreach (var err in errors)
                     Console.WriteLine($"* {err}");
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.OutputDir))
+            {
+                var aggFields = AggregateFields(dds);
+
+
+                Console.WriteLine("============================");
+                Console.WriteLine("Writing files:");
+
+                var fieldTagsPath = Path.Join(options.OutputDir, "QuickFIXn", "Fields", "FieldTags.cs");
+                System.IO.File.WriteAllText(fieldTagsPath, DDTool.Generators.GenFieldTags.Generate(aggFields));
+                Console.WriteLine($"* Wrote {fieldTagsPath}");
             }
         }
 
@@ -73,12 +94,39 @@ namespace DDTool
             return dds;
         }
 
-/*
         static List<DDField> AggregateFields(List<DataDictionary> dds)
         {
-            var fields = new List<DDField>();
-            return fields;
+            // This impl is a little wierd, but it mirrors the weird Ruby design
+            // and produces the same output.
+            // Can probably write a cleaner version later.
+
+            var rv = new List<DDField>();
+
+            var fieldNames = new List<string>();
+            foreach (var dd in dds.Where(x => !x.IsFIXT).OrderBy(x => x.Identifier))
+            {
+                foreach (var name in dd.FieldsByName.Keys)
+                {
+                    if(!fieldNames.Contains(name))
+                        fieldNames.Add(name);
+                }
+            }
+
+            foreach (var name in fieldNames)
+                rv.Add(FindField(name, dds));
+
+            return rv;
         }
-*/
+
+        static DDField FindField(string name, List<DataDictionary> dds)
+        {
+            DDField rv = null;
+            foreach (var dd in dds.Where(x => !x.IsFIXT).OrderByDescending(x => x.Identifier))
+            {
+                if(dd.FieldsByName.TryGetValue(name, out rv))
+                    return rv;
+            }
+            throw new Exception($"couldn't find field: {name}");
+        }
     }
 }
